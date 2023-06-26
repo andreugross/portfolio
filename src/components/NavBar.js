b97c16a58ca12054292dae67a458ae51ca9c1794
André Gross <andgross@gmail.com>
2023-06-26 16:53:09 -0300

    feat: create Logo and adding some styles to the buttons on NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,35 @@
 import Link from 'next/link'
 import React from 'react'
+import Logo from './Logo'
 
+const CustomLink = ({href, title, className=""}) => {
+  return(
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span className='h-[1px] inline-block w-0 bg-dark 
+      absolute left-0 -bottom-0.5
+      group-hover:w-full transition-[width] ease duration-300'
+      >&nbsp;</span>
+    </Link>
+  )
+}
 const NavBar = () => {
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
         <nav>
-            <Link href='/'>Home</Link>
-            <Link href='/'>Sobre Mim</Link>
-            <Link href='/'>Projetos</Link>
-            <Link href='/'>Artigos</Link>
+            <CustomLink href='/' title="Home" className='mr-4' />
+            <CustomLink href='/about' title="Sobre Mim" className='mx-4' />
+            <CustomLink href='/projects' title="Projetos" className='mx-4' />
+            <CustomLink href='/articles' title="Artigos" className='ml-4' />
         </nav>
-        <h2>Logo</h2>
         <nav>
             <Link href='/' target={"_blank"}>L</Link>
             <Link href='/' target={"_blank"}>G</Link>
             <Link href='/' target={"_blank"}>E</Link>
         </nav>
+        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+        <Logo />
+        </div>
         </header>
   )
 }
