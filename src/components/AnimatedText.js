7a348dab8f61fdcec64812d6dfdc10fa2b5cfd46
André Gross <andgross@gmail.com>
2023-06-26 22:49:31 -0300

    feat: create layout and add image

---
+++ src/components/AnimatedText.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AnimatedText = ({ text, className = '' }) => {
+  return (
+    <div className='w-full mx-auto py-2 flex items-center justify-center text-center
+    overflow-hidden
+    '>
+      <h1>
+        AnimatedText
+      </h1>
+    </div>
+  )
+}
+
+export default AnimatedText
