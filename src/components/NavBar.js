f5a477d2f602ae450f54b200796cb00752934558
André Gross <andgross@gmail.com>
2023-06-26 22:51:10 -0300

    fix: format documents

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,13 @@ import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
-import {LinkedInIcon, GithubIcon, GmailIcon } from './Icons'
+import { LinkedInIcon, GithubIcon, GmailIcon } from './Icons'
 import { motion } from 'framer-motion'
 
-const CustomLink = ({href, title, className=""}) => {
+const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
   console.log(router);
-  return(
+  return (
     <Link href={href} className={`${className} relative group`}>
       {title}
       <span className={`h-[1px] inline-block bg-dark 
@@ -21,33 +21,33 @@ const CustomLink = ({href, title, className=""}) => {
 const NavBar = () => {
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
-        <nav>
-            <CustomLink href='/' title="Home" className='mr-4' />
-            <CustomLink href='/about' title="Sobre Mim" className='mx-4' />
-            <CustomLink href='/projects' title="Projetos" className='mx-4' />
-            <CustomLink href='/articles' title="Artigos" className='ml-4' />
-        </nav>
-        <nav className='flex items-center justify-center flex-wrap'>
-            <motion.a href='https://twitter.com' target={"_blank"}
-            whileHover={{y:-2}}
-            whileTap={{scale:0.9}}
-            className='w-6 mr-3'
-            ><LinkedInIcon /></motion.a>
-            <motion.a href='https://twitter.com' target={"_blank"}
-            whileHover={{y:-2}}
-            whileTap={{scale:0.9}}
-            className='w-6 mx-3'
-            ><GithubIcon /></motion.a>
-            <motion.a href='https://twitter.com' target={"_blank"}
-            whileHover={{y:-2}}
-            whileTap={{scale:0.9}}
-            className='w-6 ml-3'
-            ><GmailIcon /></motion.a>
-        </nav>
-        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+      <nav>
+        <CustomLink href='/' title="Home" className='mr-4' />
+        <CustomLink href='/about' title="Sobre Mim" className='mx-4' />
+        <CustomLink href='/projects' title="Projetos" className='mx-4' />
+        <CustomLink href='/articles' title="Artigos" className='ml-4' />
+      </nav>
+      <nav className='flex items-center justify-center flex-wrap'>
+        <motion.a href='https://twitter.com' target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className='w-6 mr-3'
+        ><LinkedInIcon /></motion.a>
+        <motion.a href='https://twitter.com' target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className='w-6 mx-3'
+        ><GithubIcon /></motion.a>
+        <motion.a href='https://twitter.com' target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className='w-6 ml-3'
+        ><GmailIcon /></motion.a>
+      </nav>
+      <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo />
-        </div>
-        </header>
+      </div>
+    </header>
   )
 }
 
