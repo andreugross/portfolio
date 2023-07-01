fbae70537b3e1d4c8a620041c13f6482c12aa3f3
André Gross <andgross@gmail.com>
2023-06-30 22:40:45 -0300

    fix: adding a lasted salve on AnimatedNumbers to repeat just once

--- src/components/AnimatedNumbers.js
+++ src/components/AnimatedNumbers.js
@@ -6,7 +6,7 @@ export const AnimatedNumbers = ({value}) => {
   
     const motionValue = useMotionValue(0)
     const springValue = useSpring(motionValue, {duration: 3000})
-    const isInView = useInView(ref)
+    const isInView = useInView(ref, {once: true})
   
     useEffect(() => {
       if(isInView) {
